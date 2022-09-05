278326eee6c93c9b2db2aaa2b0bf51dfdadec102
Jesreal Arcillas <jesrealarcillas@gmail.com>
2022-09-05 23:12:08 +0800

    add reducers and actions

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+export const songListReducer = () => {
+  return [
+    { title: 'No Scrubs', duration: '4:05' },
+    { title: 'Macarena', duration: '2:30' },
+    { title: 'All Star', duration: '2:40' },
+    { title: 'I Want it that Way', duration: '3:40' },
+  ];
+};
+
+export const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
